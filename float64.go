25f2489da7924a8d84c756f214c29a7444ab6155
Aaron Schlesinger <arschles@protonmail.com>
2019-03-12 20:49:45 -0700

    Using a JSON tree internally
    Instead of ferrying bytes around. This eliminates a bytes -> JSON -> bytes round trip

--- float64.go
+++ float64.go
@@ -1,16 +1,16 @@
 package dcode
 
 import (
-	"encoding/json"
+	tree "github.com/bmatsuo/go-jsontree"
 )
 
 // Float64 decodes any JSON field into a float64
 func Float64() Decoder {
-	return newDecoder(func(b []byte) (interface{}, error) {
-		var f float64
-		if err := json.Unmarshal(b, &f); err != nil {
-			return "", err
+	return newDecoder(func(t *tree.JsonTree) (interface{}, error) {
+		ret, err := t.Number()
+		if err != nil {
+			return nil, err
 		}
-		return f, nil
+		return ret, nil
 	})
 }
